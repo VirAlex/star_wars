3c247327d8ace207e7b63fd25770720313975925
VirAlex <alexou73@gmail.com>
2020-01-02 20:03:04 +0100

    debut projet

---
+++ app/controllers/consultations_controller.rb
@@ -0,0 +1,22 @@
+class ConsultationsController < ApplicationController
+  require 'json'
+  require 'open-uri'
+  require "awesome_print"
+
+  def index
+    Consultation.destroy_all
+    url = "https://swapi.co/api/films"
+    user_serialized = open(url).read
+    user = JSON.parse(user_serialized)
+    user['results'].each do |title|
+       Consultation.create!(
+            title:    title["title"]
+            )
+    @films = Consultation.all
+    end
+  end
+
+  def show
+    @film = Consultation.find(params[:id])
+  end
+end
